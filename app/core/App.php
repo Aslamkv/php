9cd36718945e944ee22c1c955719e80825864ee9
Aslam <kv.aslam@gmail.com>
2020-05-28 09:12:00 +0530

    Initial version

---
+++ app/core/App.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+class App{
+  private function is_empty($value){
+    return trim($value)=="";
+  }
+
+  public function greet($name){
+    if($this->is_empty($name)){
+      throw new Exception("Name cannot be empty!");
+    }
+    return "Hello, $name";
+  }
+
+}
---
+++ tests/AppTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class AppTest extends TestCase{
+  public static function setUpBeforeClass(): void{
+  }
+
+  protected function setUp():void{
+  }
+
+  public function testCanCreateApp(): void{
+    $app=new App();
+    $this->assertInstanceOf(
+      App::class,
+      $app
+    );
+  }
+
+  public function testAppCanGreet(): void{
+    $app=new App();
+    $output=$app->greet("Bob");
+    $this->assertEquals(
+      "Hello, Bob",
+      $output
+    );
+  }
+
+  public function testAppCannotGreetIfNameIsEmpty(): void{
+    $this->expectExceptionMessage('Name cannot be empty!');
+    $app=new App();
+    $app->greet("");
+  }
+
+  protected function tearDown():void{
+  }
+
+  public static function tearDownAfterClass():void{
+  }
+
+}
